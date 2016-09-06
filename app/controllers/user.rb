5f6540df5211b1eebc1e0b6025e2edd191cdee63
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-09-06 10:00:37 -0700

    Finishes user controller and adds layouts and nav

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,1 +1,31 @@
-user.rb
+# GET REQUESTS ======================================================
+
+# new user sign up
+get '/register' do
+  @user = User.new
+  erb :'users/new'
+end
+
+# show user profile
+get '/profile' do
+  erb :'/users/profile'
+end
+
+
+# POST REQUESTS =====================================================
+
+# create new user
+post '/users' do
+  
+  @user = User.create(params[:user])
+  
+  if @user.save
+    session[:user_id] = @user.id
+    redirect :'/profile'
+  else
+    status 422
+    @errors = @user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
