ead9c256cf6ddf340edbbfc24c42785781c20fc9
Rachel Munoz <email": null>
2016-09-09 10:11:33 -0700

    Adds navbar to layout, adds password becryption and user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+
+  validates :name, :email, :hashed_password, { presence: true}
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+
+  def password=(password)
+    @password = BCrypt::Password.create(password)
+    self.hashed_password = @password
+  end
+
+  def self.authentication(email, password)
+    @user = User.find_by(email: email)
+    return @user if @user && @user.passsword == password
+  end
+
+
+
+
+
+
 end
